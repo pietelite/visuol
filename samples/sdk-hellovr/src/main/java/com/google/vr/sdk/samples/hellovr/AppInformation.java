9a7bfaefcaa14c6bc6b579cca8399ee4813b89d1
pietelite <pieter2@illinois.edu>
2019-03-18 13:44:17 -0500

    Made the Information page from the launch page

---
+++ samples/sdk-hellovr/src/main/java/com/google/vr/sdk/samples/hellovr/AppInformation.java
@@ -0,0 +1,14 @@
+package com.google.vr.sdk.samples.hellovr;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AppInformation extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_information);
+    }
+
+}
